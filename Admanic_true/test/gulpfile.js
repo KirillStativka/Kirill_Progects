cd2493fb9c1dfdedbd83ecd39808be68cb078496
Kirill Stativka <ssko_17@mail.ru>
2017-02-02 00:24:33 +0200

    Latest update

---
+++ Admanic_true/test/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/main.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/main.sass', ['sass']);
+
+});
